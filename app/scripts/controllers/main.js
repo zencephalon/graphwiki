decd05ec6de208395760a30cfe7fc3e5dee10124
Matthew Bunday <zenceph@gmail.com>
2014-03-15 22:57:39 -0400

    Yeoman scaffold.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wikigraphApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
